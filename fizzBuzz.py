2afa0fc5d4ae124819822b8b3f722857bd9fa52b
MarcellaLarissa <56004783+MarcellaLarissa@users.noreply.github.com>
2021-05-30 19:32:52 -0700

    updated code: input that is a multiple of 3 AND 5 returns 'fizzbuzz'

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,9 +1,14 @@
 def fb(nums):
     
+    if ((nums % 5 == 0) and (nums % 3 == 0)):
+        return "fizzbuzz"
+    
     if (nums % 3 == 0):
         return "fizz"
+    
     if (nums % 5 == 0):
         return "buzz"
+
     else:
         return nums
     
