9a5d35b4431e254f376850b8a6d02aa555483d28
MarcellaLarissa <56004783+MarcellaLarissa@users.noreply.github.com>
2021-05-30 18:56:06 -0700

    Wrote second test, asserts that input 1 will return 2. Failed

--- test_fizzBuzz.py
+++ test_fizzBuzz.py
@@ -6,6 +6,9 @@ class TestCase(unittest.TestCase):
     #test one
     def test_leap(self):
         self.assertEqual(fizzBuzz.nums(1), 1)
+    #test two
+    def test_leap2(self):
+        self.assertEqual(fizzBuzz.nums(1), 2)
         
 if __name__ == '__main__':
     unittest.main()
