03cd25e688f4a0ddbd3b00f9bb1f3fe7965ecd06
MarcellaLarissa <56004783+MarcellaLarissa@users.noreply.github.com>
2021-05-30 20:45:40 -0700

    Updated Code: input that is divisible by 4 but not by 100 will return 'leap year'

--- leapTDD.py
+++ leapTDD.py
@@ -1,10 +1,10 @@
 def leap(year):
     #Years that are evenly divisible by 4
     if(year%4 == 0):
-                #case of leap years less than year 100
+        #case of leap years less than year 100
         if(year < 100):
             return "leap year"
-                #case of years greater than 100 -- possible leap year
+        #case of years greater than 100 -- possible leap year
         elif(year%100 == 0):    
                         # case of years are evenly divisible by 400 -- leap year
             if(year%400 == 0):
@@ -13,7 +13,9 @@ def leap(year):
             #years that are evenly divisible by 100 -- no leap year
             else:
                 return "NOT a leap year"  
-        
+        #years that are evenly divisible by 4 but not 100 -- leap year
+        else:
+            return "leap year"
     #Years that are NOT evenly divisible by 4            
     else:
         return "NOT a leap year"
