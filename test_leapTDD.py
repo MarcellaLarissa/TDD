5978a5c4b859fee16ce35002b68582d213c024bb
MarcellaLarissa <56004783+MarcellaLarissa@users.noreply.github.com>
2021-05-30 20:23:02 -0700

    Third Test: assert 40 is a leap year: passed

--- test_leapTDD.py
+++ test_leapTDD.py
@@ -6,10 +6,12 @@ class TestCase(unittest.TestCase):
     #test one
     def test_leap(self):
         self.assertEqual(leapTDD.leap(4), "leap year")
-    #test one
+    #test two
     def test_leap2(self):
         self.assertEqual(leapTDD.leap(5), "NOT a leap year")
-        
+    #test three
+    def test_leap3(self):
+        self.assertEqual(leapTDD.leap(40), "leap year")        
 
         
    
