92b3f2c25890c2f6e827ba8b4f8deaabfa0a24d5
MarcellaLarissa <56004783+MarcellaLarissa@users.noreply.github.com>
2021-05-30 20:14:13 -0700

    Updated Code: input that is not divisble by 4 will return 'not a leap year'

--- leapTDD.py
+++ leapTDD.py
@@ -2,3 +2,9 @@ def leap(year):
     #Years that are evenly divisible by 4
     if(year%4 == 0):
         return "leap year"
+        
+        
+    #Years that are NOT evenly divisible by 4            
+    else:
+        return "NOT a leap year"
+  
